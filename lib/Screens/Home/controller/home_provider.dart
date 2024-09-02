c9a962aa61abb06bda3673cbf4bff9c3b55f58c3
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-02 09:02:05 +0530

    Using the provider

--- lib/Screens/Home/controller/home_provider.dart
+++ lib/Screens/Home/controller/home_provider.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomeProvider extends ChangeNotifier {
-  
+  Future<void> textToImage() async {
+    String engine_id = "stable-diffusion-v1-6";
+    String api_host = 'https://api.stability.ai';
+    String api_key = "STABILITY_API_KEY";
+
+    final response = 
+  }
 }
